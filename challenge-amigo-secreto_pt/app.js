ddf71486681879e2605287389ba922fd0e89469b
Gabriel Schneider <ogabrielschneider@gmail.com>
2025-02-07 18:03:45 -0300

    Criação do ".gitignore"; .js- Criação de uma lista para armazenar nomes e de uma função para adicionar nomes, começo da criação de uma função para atualizar a lista de interface; .html- Alteração do id da lista

--- challenge-amigo-secreto_pt/app.js
+++ challenge-amigo-secreto_pt/app.js
@@ -1,3 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
+//Aqui você deverá desenvolver a lógica para resolver o problema.
 
-//Meu primeiro commit
+// #1. Crie um array para armazenar os nomes
+
+let listaDeNomes = [];
+
+
+// #2. Implementa uma função para agregar amigos
+
+function adicionarNomes () {
+    let nome = document.querySelector('input').value;
+    if (nome == '' || nome != String) {
+        alert('Por favor, insira um nome.');
+    } else {
+        listaDeNomes.push(nome);
+    }
+    document.querySelector('input').value = '';
+}
+adicionarNomes();
+
+// #3. Implementa uma função para atualizar a lista de amigos
+
+function atualizarListaDeNomes () {
+    listaInterface = document.getElementById('ListaDeNomesInterface');
+    listaInterface.innerHTML = '';
+    //Continuar
+}
